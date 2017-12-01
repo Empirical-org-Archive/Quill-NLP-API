75498d1595ea3bd919e96ad628c46125ee248364
Donald McKendrick <ddmckendrick@gmail.com>
2017-12-01 16:32:53 -0500

    Update server script

--- application.py
+++ application.py
@@ -13,8 +13,15 @@ def ping():
 
 @app.route("/get_POS", methods=["POST"])
 def get_pos():
-  return jsonify({'text': libs.pos.get_POS(request.form['text'])})
+    return jsonify({'text': libs.pos.get_POS(request.form['text'])})
 
 @app.route("/sentence_or_not", methods=["POST"])
 def sentence_or_not():
-  return jsonify({'text': libs.ai.test_sentence(request.form['text'])})
+    return jsonify({'text': libs.ai.test_sentence(request.form['text'])})
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
