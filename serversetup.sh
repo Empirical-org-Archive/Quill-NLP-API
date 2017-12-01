770c49b6774f929ddd7814a7bc0527445b21edde
Donald McKendrick <ddmckendrick@gmail.com>
2017-12-01 14:08:44 -0500

    add requirements.txt

---
+++ serversetup.sh
@@ -0,0 +1,13 @@
+# Based on https://www.thoughtvector.io/blog/deployment-with-anaconda/
+
+sudo yum install git
+curl https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -o miniconda.sh
+sh miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+git clone https://github.com/empirical-org/Quill-NLP-API.git
+cd Quill-NLP-API/
+conda create -n app spacy Flask gunicorn tensorflow nltk python=3
+source activate app
+pip install tflearn
+python -m spacy download en
+FLASK_APP=server.py flask run # and boom goes the dynamite
